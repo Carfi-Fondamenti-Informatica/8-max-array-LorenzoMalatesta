2e96e18b5557d8aeb4bb7210ded7221d11c84f53
LorenzoMalatesta <100367512+LorenzoMalatesta@users.noreply.github.com>
2022-05-04 10:14:02 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    int d;
+    cin >> d;
+    float a[d];
+    for (int i=0; i<d; i++) {
+        a[i]=0;
+    }
+    for (int i=0; i<d; i++) {
+        cin >> a[i];
+    }
+    cout << max (a, d);
+    return 0;
 }
