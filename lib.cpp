082f3973ba3ede7bfc1974f29857ae2068f65938
LorenzoMalatesta <100367512+LorenzoMalatesta@users.noreply.github.com>
2022-05-04 10:13:38 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,16 @@
+//
+// Created by Lorenzo Malatesta on 04/05/22.
+//
 
+#include "lib.h"
+
+float max (float num[], int dim) {
+    int i=0;
+    float ris = num[i];
+    for (i=1; i<dim; i++) {
+        if (num[i] > num[i-1]) {
+            ris = num[i];
+        }
+    }
+    return ris;
+}
